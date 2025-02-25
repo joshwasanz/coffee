4182be393913b109c57107dd3caa69e4b3533e3d
Joshwa Sanz <joshwasanz@gmail.com>
2025-02-25 09:31:01 -0500

    rest framework issue solved in settings.py

--- coffee/coffee/settings.py
+++ coffee/coffee/settings.py
@@ -64,6 +64,18 @@ INSTALLED_APPS = [
 
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
