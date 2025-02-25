2e833340e4f89d97e64b110b89e62de478e6552c
Joshwa Sanz <joshwasanz@gmail.com>
2025-02-25 13:54:16 -0500

    resolving issues

--- coffee/posts/api/views.py
+++ coffee/posts/api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import status
 from rest_framework.decorators import api_view,permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework.response import Response
 from posts.models import Post
 from .serializers import PostSerializer
-
+from users.models import User
 
 @api_view(['GET','POST'])
+@permission_classes([AllowAny])
 def post_list(request):
+
+    default_author = User.objects.get(username="joshwa")
 
     if request.method == 'GET':
         posts = Post.objects.all().order_by('-created_at')
@@ -18,7 +21,7 @@ def post_list(request):
         serializer = PostSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save(author=request.user,topics=request.data['topics'])
+            serializer.save(author=request.user if request.user.is_authenticated else default_author,topics=request.data['topics'])
             print(request.data['topics'])
 
             return Response(serializer.data,status=status.HTTP_201_CREATED)
